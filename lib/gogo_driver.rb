0224ad3698cef7df1ccb78a94c78f56c7be4fa37
gogotanaka <mail@tanakakazuki.com>
2014-07-06 13:01:45 -0700

    Implement main module

--- lib/gogo_driver.rb
+++ lib/gogo_driver.rb
@@ -2,5 +2,15 @@ require "gogo_driver/version"
 require "gogo_driver/entity"
 
 module GogoDriver
+  def go(url='https://www.google.com')
+    @entity = Entity.new unless @entity
+    @entity.go(url)
+  end
 
+  def method_missing(method, *args, &block)
+    @entity.respond_to?(method) ? @entity.send(method, *args, &block) : super
+  rescue Errno::ECONNREFUSED
+    go # Remind past url ...
+    @entity.send(method, *args, &block)
+  end
 end
