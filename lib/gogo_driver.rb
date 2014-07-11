5e6ac32d426e635de3ea1137c51effecbff71984
gogotanaka <mail@tanakakazuki.com>
2014-07-10 22:05:41 -0700

    Some improvement

--- lib/gogo_driver.rb
+++ lib/gogo_driver.rb
@@ -5,12 +5,14 @@ module GogoDriver
   def go(url='https://www.google.com')
     @entity = Entity.new unless @entity
     @entity.go(url)
+    @last_url = url
   end
+  module_function :go
 
   def method_missing(method, *args, &block)
     @entity.respond_to?(method) ? @entity.send(method, *args, &block) : super
   rescue Errno::ECONNREFUSED
-    go # Remind past url ...
+    go(@last_url)
     @entity.send(method, *args, &block)
   end
 end
