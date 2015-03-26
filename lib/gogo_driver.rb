4851ad2abe610f4045bcadc8f5f767ed14298d5c
gogotanaka <mail@tanakakazuki.com>
2015-03-25 22:36:15 -0700

    v0.1.1

--- lib/gogo_driver.rb
+++ lib/gogo_driver.rb
@@ -19,6 +19,10 @@ class GogoDriver
     @driver.find_element(css: selector)
   end
 
+  def finds(selector)
+    @driver.find_elements(css: selector)
+  end
+
   def has?(selector)
     !!find(selector)
   rescue Selenium::WebDriver::Error::NoSuchElementError
@@ -41,5 +45,20 @@ class GogoDriver
 
   def method_missing(method, *args, &block)
     @driver.respond_to?(method) ? @driver.send(method, *args, &block) : super
+  end
+end
+
+class Selenium::WebDriver::Element
+  def fill(text)
+    $focus = self
+    send_key(text)
+  end
+
+  def find(selector)
+    find_element(css: selector)
+  end
+
+  def finds(selector)
+    find_elements(css: selector)
   end
 end
