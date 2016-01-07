c1265eaf2f2aa17be9256ff0ce141aafd9326c23
gogotanaka <mail@tanakakazuki.com>
2016-01-07 16:29:49 +0900

    Compress common inteface

--- lib/smart_driver/common_interface.rb
+++ lib/smart_driver/common_interface.rb
@@ -1,5 +1,37 @@
 class SmartDriver
   module CommonInterface
+    def find(selector)
+      logging :info, "find #{selector}..."
+      self.find_element(css: selector)
+    rescue Selenium::WebDriver::Error::NoSuchElementError
+      logging :fail, "#{selector} cannot be found..."
+      nil
+    end
+
+    def finds(selector)
+      logging :info, "finds #{selector}..."
+      self.find_elements(css: selector)
+    rescue Selenium::WebDriver::Error::NoSuchElementError
+      logging :fail, "#{selector} cannot be found..."
+      nil
+    end
+
+    def find_text(text)
+      logging :info, "find text '#{text}'..."
+      self.find_element({xpath: "//*[text()[contains(.,\"#{text}\")]]"})
+    rescue Selenium::WebDriver::Error::NoSuchElementError
+      logging :fail, "text '#{text}' cannot be found"
+      nil
+    end
+
+    def finds_text(text)
+      logging :info, "finds text '#{text}'..."
+      self.find_elements({xpath: "//*[text()[contains(.,\"#{text}\")]]"})
+    rescue Selenium::WebDriver::Error::NoSuchElementError
+      logging :fail, "text #{text} cannot be found"
+      nil
+    end
+
     def to_html
       attribute("outerHTML")
     end
